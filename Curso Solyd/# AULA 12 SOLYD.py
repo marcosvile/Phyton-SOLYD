b612ba65011413896a7dd931c353fa877c92d0f6
marcosvile <marcosvilelateixeira.com>
2021-11-10 21:58:47 -0300

    compreendendo requests

--- Curso Solyd/# AULA 12 SOLYD.py
+++ Curso Solyd/# AULA 12 SOLYD.py
@@ -6,4 +6,20 @@ import sys
 import urllib
 import requests
 
+cabecalho = {"User-agent": "Windows 18",
+             'Referer': "https://google.com",
+             'cf-ipcountry': "BK"}
+
+meus_cookies = {"Ultima-visita": '10-10-1994'}
+texto = None
+
+try:
+    requisicao = requests.post('https://putsreq.com/WxMoXb7ziLSCCNcARF0s', headers=cabecalho, cookies=meus_cookies)                
+    texto = requisicao.text
+    
+except Exception as e:
+    print("requisicao deu erro: ", e)
+
+print(texto)
+
 
