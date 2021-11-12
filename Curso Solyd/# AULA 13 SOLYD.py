ac9e4425c3016846781aca50b3bb80dc818ec940
marcosvile <marcosvilelateixeira.com>
2021-11-12 14:56:42 -0300

    consumindo api de buscar filmes obs: omdb nao esta funcionando mesmo com key

---
+++ Curso Solyd/# AULA 13 SOLYD.py
@@ -0,0 +1,41 @@
+# AULA 13 SOLYD
+
+# Consumo de API E Json
+
+import requests
+import json
+
+def requisicao(reqtitle):
+
+    try:
+
+#http://www.omdbapi.com/?apikey=[yourkey]&
+
+
+        req = requests.get("http://www.omdbapi.com/?apikey=d00f3f58&=" + reqtitle)
+        dic = json.loads(req.text)
+        return dic
+
+    except:
+        print( "Erro na conexão ")    
+        return None
+
+def printar(filme):
+    print('titulo: ', filme["Title"])
+
+sair = False
+while not sair:
+    op = input("escreva um nome de uma iflme ou SAIR para fechar: ")
+    
+    if op == "sair":
+        sair = True
+        print("saindo")
+        
+    else:
+        filme = requisicao(op)
+        if filme['Response'] == 'False':
+            print('nao encontrado')
+        else:
+            printar(filme)
+            
+            
