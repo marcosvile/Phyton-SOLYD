64f920e89166d24e7938ecee88dd1b85516a4337
marcosvile <marcosvilelateixeira.com>
2021-11-15 20:23:24 -0300

    sobre expressoes regulares

--- Curso Solyd/# AULA 14 SOLYD.py
+++ Curso Solyd/# AULA 14 SOLYD.py
@@ -2,3 +2,17 @@
 
 # Expressões regulares
 
+# padrao de email - email@tempest.com.br
+# Expressões regulares também chamados de regex
+
+import re
+
+teste_search = 'o gato é bonito'
+
+padrao = re.search(r'tempest', teste_search) #r -> transforma o parametro de busra em RAW STRING
+
+if padrao:
+    print(padrao.group())
+else:
+    print("padrao nao encontrado")
+
