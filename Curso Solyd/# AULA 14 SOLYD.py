7abee2822fd57dca6a26bf4fc08872c124b72743
marcosvile <marcosvilelateixeira.com>
2021-11-15 20:38:52 -0300

    entendendo estruturas de email

--- Curso Solyd/# AULA 14 SOLYD.py
+++ Curso Solyd/# AULA 14 SOLYD.py
@@ -2,17 +2,34 @@
 
 # Expressões regulares
 
-# padrao de email - email@tempest.com.br
+# padrao de email - email@dominio.com.br
 # Expressões regulares também chamados de regex
 
 import re
 
-teste_search = 'o gato é bonito'
+teste_search = 'o gato é bonito, a gata, o gatao.com os gatinhos'
 
 padrao = re.search(r'tempest', teste_search) #r -> transforma o parametro de busra em RAW STRING
+                                             #\w -> busca caracteres do tipo texto, numeros e caracters
+                                             # sem considerar espaços em brancos, considerando a sequencia \w 
 
 if padrao:
     print(padrao.group())
 else:
     print("padrao nao encontrado")
 
+padrao = re.findall(r'gat\w+\.', teste_search)
+
+if padrao:
+    print(padrao)
+else:
+    print("padrao nao encontrado")
+    
+    padrao = re.findall(r'[gat]+\w', teste_search)
+
+if padrao:
+    print(padrao)
+else:
+    print("padrao nao encontrado")
+    
+    
