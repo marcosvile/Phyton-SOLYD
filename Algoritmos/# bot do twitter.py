c1cba7200adcecb39340f8bc2fe61ab5b6d002d5
marcosvile <marcosvilelateixeira.com>
2021-11-19 14:56:11 -0300

    iniciando algoritmo de bot

---
+++ Algoritmos/# bot do twitter.py
@@ -0,0 +1,22 @@
+# Criação d eum bot que posta automatico no twitter
+
+# bot_conselhogratis
+
+# cfTGYgcNwFiIBlWKZGgRD2sHr apikey
+# 2397hWVVkhFCRPFK1K6LNutyL6Tc0RYDAqLLvgJZwUJ9nRhhzP api secret key
+# AAAAAAAAAAAAAAAAAAAAAEDjVwEAAAAAhUkTCeXLpfdXyU51nkMmiItMVCk%3DAgEHQ2RpKh8MKQ9jk9z11lvjeC1nWGvpOXlXxzwYqbJxp8DYd1 baren token
+
+consumerkey = 'cfTGYgcNwFiIBlWKZGgRD2sHr1'
+keysecret = '2397hWVVkhFCRPFK1K6LNutyL6Tc0RYDAqLLvgJZwUJ9nRhhzP1'
+consumertoken = 'AAAAAAAAAAAAAAAAAAAAAAEDjVwEAAAAAhUkTCeXLpfdXyU51nkMmiItMVCk%3DAgEHQ2RpKh8MKQ9jk9z11lvjeC1nWGvpOXlXxzwYqbJxp8DYd1'
+
+import re
+import json
+import oauth2 as oauth
+#import TwitterAPI
+
+consumer = oauth2.consumer (consumerkey, consumertoken)
+
+
+
+
