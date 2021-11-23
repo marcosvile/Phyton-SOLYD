0559a777ff600ff8bdb5a42bf0ba76fe3e3185a4
marcosvile <marcosvilelateixeira.com>
2021-11-22 23:14:45 -0300

    verificação

--- Curso Solyd/# AuLA 17 SOLYD.py
+++ Curso Solyd/# AuLA 17 SOLYD.py
@@ -2,3 +2,14 @@
 
 # twitter post
 
+# api key yqnoL1V281fPfIftWaRYZzB84
+
+# api secret key WXAzb1hL5TrR3wZrhzJvPm6y9qqCYUOHdgxSpCZRN8tw4LteOu
+
+# bearren tokent AAAAAAAAAAAAAAAAAAAAAM5TWAEAAAAAMKR7BMHaS6%2FIL0ghqFNO40SW2y0%3DVefJOitMBv8p50HGtmdXLrPSXPTRMYmwb4xNX7KWBBxLg19rhQ
+
+# access token 1457323725305614337-7NFP8QonflUrlceDHCJ5d4m9Psqqhy
+
+# access token secret r120q61cq2sKHLUhqIwlB76S0Wr4Y6EJyGaN8jsC6iZ1b 
+
+
